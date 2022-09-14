21dd0ee935fb6a5012eb7f0320c532dad9d2233d
kdbajj <k.dbaj@vp.pl>
2022-09-14 16:09:18 +0200

    LECTURE: Values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -3,3 +3,16 @@
 
 // LECTURE: Values and Variables
 console.log(50 - 45 + 3);
+console.log("Alice");
+console.log(32);
+
+let firstName = "abc"; // przypisanie wartości do zmiennej
+console.log(firstName);
+console.log(firstName);
+let PI = 3.1415; // możemy nazwać zmienną samymi dużymi literami
+
+let country = 'Poland';
+let continent = 'Europe';
+let population = '40';
+console.log(country, continent, population);
+
