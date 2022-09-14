794bd80a72a0ffc9c6fb7a1441539be5ce219bd0
kdbajj <k.dbaj@vp.pl>
2022-09-14 16:08:31 +0200

    LECTURE: A Brief Introduction to JavaScript

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,5 @@
-let js = "amazing";
-if (js === "amazing") alert("javascript is fun!");
+// let js = "amazing";
+// if (js === "amazing") alert("javascript is fun!");
+
+// LECTURE: Values and Variables
 console.log(50 - 45 + 3);
