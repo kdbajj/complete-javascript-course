fe750043c6a46eb16ced9b096deba46a2d086eea
Karolina Dbaj <k.dbaj@vp.pl>
2022-09-29 09:51:32 +0200

    The switch Statement

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -215,4 +215,40 @@ if (isMinimum = true){
     console.log(`Both teams had score lower than 100 :((`);
 }
 
+//The switch Statement
+
+const day = 'thursday';
+
+switch(day) {
+    case 'monday':// day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meet up');
+            break;
+        case 'tuesday':
+            console.log('Prepare theory videos');
+            break;
+        case 'wensday':
+        case 'thursday':
+        console.log('Write code examples');
+            break;
+        case 'friday':
+        console.log('Record videos');
+            break;
+        case 'saturday':
+        case 'sunday':
+            console.log('Enjoy the weekend :D');
+        default:
+            console.log('Not a valid day!');
+}
+
+if (day === 'monday')
+{
+    console.log('Plan course structure');
+    console.log('Go to coding meet up');
+}
+else if (day=== "tuesday") console.log('Prepare theory videos');
+else if (day === 'wensday' || day === 'thursday') console.log('Write code examples');
+else if (day === 'friday') console.log('Record videos');
+else if (day === 'saturday' || day === 'sunday') console.log('Enjoy the weekend :D');
+else console.log('Not a valid day!');
 
