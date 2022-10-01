ddc1aacd98dc70cf5c7ab5e7414d762309dda10a
Karolina Dbaj <k.dbaj@vp.pl>
2022-10-01 14:07:18 +0200

    Coding Challenge #4

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -279,3 +279,4 @@ tip = bill >= 50 && bill <= 300 ? ((bill * 15 )/ 100 ) : ((bill * 20 )/100);
 console.log(tip);
 
 console.log((`The bil was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`));
+
