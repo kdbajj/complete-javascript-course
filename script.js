0eb4199c992d8ffb76f185ab77956d11e962df3a
Karolina Dbaj <k.dbaj@vp.pl>
2022-10-03 16:14:16 +0200

    Functions

--- script.js
+++ script.js
@@ -6,3 +6,25 @@
  if (hasDriversLicense) console.log(`I can drive :D`);
 
  //Functions
+
+ function logger (){
+    console.log('My name is Karolina :))');
+ }
+
+ // wywołanie funkcji
+ logger ();
+ logger ();
+ logger ();
+
+function fruitProcessor(apples, oranges){
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5,0);
+console.log(appleJuice);
+console.log(fruitProcessor(5,3));
+
+const appleOrangeJuice = fruitProcessor(2,4);
+console.log(appleOrangeJuice);
