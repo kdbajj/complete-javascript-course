c38153d3a7cca78356897767124a2e53a112db07
Karolina Dbaj <k.dbaj@vp.pl>
2022-10-03 20:55:30 +0200

    Function Declarations vs. Expressions

--- script.js
+++ script.js
@@ -28,3 +28,21 @@ console.log(fruitProcessor(5,3));
 
 const appleOrangeJuice = fruitProcessor(2,4);
 console.log(appleOrangeJuice);
+
+//Function Declarations vs. Expressions
+
+// Function declarations
+const age1 = calAge1(1991)
+
+function calAge1(birthYear){
+    return 2022-birthYear;
+}
+
+// Function expression
+
+const calAge2 = function (birthYear){
+    return 2022 - birthYear;
+}
+const age2 = calAge2(2002);
+
+console.log(age1, age2);
