471b260c1e8ada004836ac2526a4e111e58d6f02
Karolina Dbaj <k.dbaj@vp.pl>
2022-10-10 22:44:21 +0200

    Coding Challenge #2

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,20 +1,20 @@
-'use strict';
- let hasDriversLicense = false;
- const passTest = true;
+"use strict";
+let hasDriversLicense = false;
+const passTest = true;
 
- if (passTest) hasDriversLicense = true;
- if (hasDriversLicense) console.log(`I can drive :D`);
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log(`I can drive :D`);
 
- //Functions
+//Functions
 
- function logger (){
-    console.log('My name is Karolina :))');
- }
+function logger() {
+  console.log("My name is Karolina :))");
+}
 
- // wywołanie funkcji
- logger ();
- logger ();
- logger ();
+// wywołanie funkcji
+logger();
+logger();
+logger();
 /*
 function fruitProcessor(apples, oranges){
     console.log(apples, oranges);
@@ -32,24 +32,24 @@ console.log(appleOrangeJuice);
 //Function Declarations vs. Expressions
 
 // Function declarations
-const age1 = calAge1(1991)
+const age1 = calAge1(1991);
 
-function calAge1(birthYear){
-    return 2022-birthYear;
+function calAge1(birthYear) {
+  return 2022 - birthYear;
 }
 
 // Function expression
 
-const calAge2 = function (birthYear){
-    return 2022 - birthYear;
-}
+const calAge2 = function (birthYear) {
+  return 2022 - birthYear;
+};
 const age2 = calAge2(2002);
 
 console.log(age1, age2);
 
 // Arrow Functions
 
-const calcAge3 = birthYear => 2037 - birthYear;
+const calcAge3 = (birthYear) => 2037 - birthYear;
 const age3 = calcAge3(1991);
 console.log(age3);
 /*
@@ -67,20 +67,20 @@ console.log(yearsUntilRetirement(1980, 'Bob'));
 
 // Functions Calling Other Functions
 
-function cutFruitPieces(fruit){
-    return fruit * 4;
+function cutFruitPieces(fruit) {
+  return fruit * 4;
 }
 
-function fruitProcessor(apples, oranges){
-    const applePieces = cutFruitPieces(apples);
-    const orangePieces = cutFruitPieces(oranges);
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
 
-    const juice = `Juice with ${applePieces} pieces apples and ${orangePieces} oranges.`;
-    return juice;
+  const juice = `Juice with ${applePieces} pieces apples and ${orangePieces} oranges.`;
+  return juice;
 }
-console.log(fruitProcessor(2,3));
+console.log(fruitProcessor(2, 3));
 
-//Reviewing Functions 
+//Reviewing Functions
 
 /*const calcAge = function(birthYear){
     return 2022 - birthYear;
@@ -106,78 +106,75 @@ console.log(yearsUntilRetirement(1888, 'John'));
 
 //Coding Challenge #1
 
-const calcAverage = (score1, score2, score3) => (score1,score2, score3)/3;
+const calcAverage = (score1, score2, score3) => (score1, score2, score3) / 3;
 
 let teamDolphins = calcAverage(44, 23, 71);
-let teamCoalas = calcAverage(65, 54,49);
+let teamCoalas = calcAverage(65, 54, 49);
 console.log(teamCoalas, teamDolphins);
 
+const checkWinner = function (avgCoalas, avgDolphins) {
+  if (avgCoalas >= 2 * avgDolphins) {
+    console.log(
+      `The winner is team Coalas!!!! They win ${avgCoalas} vs ${avgDolphins}.`
+    );
+  } else if (avgDolphins >= 2 * avgCoalas) {
+    console.log(
+      `The winner is team Dolphins!!!! They win ${avgDolphins} vs ${avgCoalas}.`
+    );
 
-const checkWinner = function (avgCoalas, avgDolphins){
-
-   if (avgCoalas >= 2 * avgDolphins){
-       console.log(`The winner is team Coalas!!!! They win ${avgCoalas} vs ${avgDolphins}.`);
-       
-   } else if(avgDolphins >= 2 * avgCoalas ){
-    console.log(`The winner is team Dolphins!!!! They win ${avgDolphins} vs ${avgCoalas}.`);
-   
-   return teamCoalas,teamDolphins;
-   }
-   else {
+    return teamCoalas, teamDolphins;
+  } else {
     console.log(`No one won`);
-   }
-  
-}
-checkWinner(teamCoalas,teamDolphins);
+  }
+};
+checkWinner(teamCoalas, teamDolphins);
 
 // Introduction to Arrays
 
-const friend1 = 'Michael';
-const friend2 = 'John';
-const friend3 = 'John';
+const friend1 = "Michael";
+const friend2 = "John";
+const friend3 = "John";
 
-const friends = ['Michael', 'John', 'Peter'];
+const friends = ["Michael", "John", "Peter"];
 console.log(friends);
-
-
 
 console.log(friends[0]);
 console.log(friends[2]);
 
 console.log(friends.length);
-console.log(friends[friends.length -1]);
+console.log(friends[friends.length - 1]);
 
-friends[2] = 'Jay';
+friends[2] = "Jay";
 console.log(friends);
 
-let firstName = 'karolina';
-const karolina = [firstName, 'dbaj', 2022-2002, 'student',friends];
-console.log(karolina); 
+let firstName = "karolina";
+const karolina = [firstName, "dbaj", 2022 - 2002, "student", friends];
+console.log(karolina);
 
 // Exercise
-const calcAge = function(birthYear){
-    return 2022 - birthYear;
-}
+const calcAge = function (birthYear) {
+  return 2022 - birthYear;
+};
 
 const years = [1990, 1930, 1232, 1999];
 
 const age = calcAge(years[0]);
 const age11 = calcAge(years[1]);
 const age22 = calcAge(years[2]);
-console.log(age11,age22,age);
+console.log(age11, age22, age);
 
-const ages = [calcAge(years[0]),calcAge(years[1]),calcAge(years[2])];
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[2])];
 console.log(ages);
 
 // Basic Array Operations (Methods)
 
 // Add elements
-const friends1 = ['Michael', 'John', 'Peter'];
-const newLength = friends1.push('Jay');
+const friends1 = ["Michael", "John", "Peter"];
+const newLength = friends1.push("Jay");
 console.log(friends1);
 console.log(newLength);
 
-friends.unshift('John');
+friends.unshift("John");
 console.log(friends1);
 
 // Remove elements
@@ -189,14 +186,44 @@ console.log(friends1); // Last element
 friends1.shift();
 console.log(friends1); // First element
 
-console.log(friends1.indexOf('John'));
-console.log(friends1.indexOf('Bob'));
+console.log(friends1.indexOf("John"));
+console.log(friends1.indexOf("Bob"));
 
 friends1.push(2223);
-console.log(friends1.includes('John'));
-console.log(friends1.includes('Bob'));
-console.log(friends1.includes('2223'));
+console.log(friends1.includes("John"));
+console.log(friends1.includes("Bob"));
+console.log(friends1.includes("2223"));
 
-if (friends1.includes('Peter'));{
-    console.log('You have a friend called Peter :D')
+if (friends1.includes("Peter"));
+{
+  console.log("You have a friend called Peter :D");
 }
+
+// Coding Challenge #2
+
+// 1
+
+let billValue = 50;
+
+function calcTip(billValue) {
+  let tip;
+  if (billValue > 50 && billValue < 300) {
+    tip = (billValue * 15) / 100;
+    console.log(`The tip is ${tip} :))`);
+  } else {
+    tip = (billValue * 20) / 100;
+    console.log(`The tip is ${tip} :))`);
+  }
+  return tip;
+}
+
+// 2
+const bills = [125, 555, 44];
+
+// 3
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(bills, tips);
+
+// 4
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total);
