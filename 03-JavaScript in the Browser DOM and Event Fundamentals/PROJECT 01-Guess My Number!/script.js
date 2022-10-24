d8bb5b878292ecc5ef378deb4dbb6995da1fbef1
Karolina Dbaj <k.dbaj@vp.pl>
2022-10-24 22:52:27 +0200

    Selecting and Manipulating Elements

--- 03-JavaScript in the Browser DOM and Event Fundamentals/PROJECT 01-Guess My Number!/script.js
+++ 03-JavaScript in the Browser DOM and Event Fundamentals/PROJECT 01-Guess My Number!/script.js
@@ -1,3 +1,12 @@
 "use strict";
-//70. PROJECT #1: Guess My Number!
+//PROJECT #1: Guess My Number!
 console.log(document.querySelector(".message").textContent);
+
+//Selecting and Manipulating Elements
+document.querySelector(".message").textContent = "Correct Number!🎉";
+
+document.querySelector(".number").textContent = "12";
+document.querySelector(".score").textContent = 3;
+
+document.querySelector(".guess").value = 23;
+console.log((document.querySelector(".guess").value = 23));
