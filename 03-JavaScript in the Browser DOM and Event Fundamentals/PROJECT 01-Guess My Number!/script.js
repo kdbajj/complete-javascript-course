01f0d2bd825359efeace44aae5893fe46bb4fd68
Karolina Dbaj <k.dbaj@vp.pl>
2022-10-24 23:08:40 +0200

    Handling Click Events

--- 03-JavaScript in the Browser DOM and Event Fundamentals/PROJECT 01-Guess My Number!/script.js
+++ 03-JavaScript in the Browser DOM and Event Fundamentals/PROJECT 01-Guess My Number!/script.js
@@ -1,5 +1,5 @@
 "use strict";
-//PROJECT #1: Guess My Number!
+/* //PROJECT #1: Guess My Number!
 console.log(document.querySelector(".message").textContent);
 
 //Selecting and Manipulating Elements
@@ -10,3 +10,13 @@ document.querySelector(".score").textContent = 3;
 
 document.querySelector(".guess").value = 23;
 console.log((document.querySelector(".guess").value = 23));
+*/
+
+//Handling Click Events
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number!🛑";
+  }
+});
