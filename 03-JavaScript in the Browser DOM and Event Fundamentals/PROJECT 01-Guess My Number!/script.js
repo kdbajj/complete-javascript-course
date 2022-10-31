78fb3eb5aefafa3b57319ca259582fdb01d7e59c
Karolina Dbaj <k.dbaj@vp.pl>
2022-10-31 20:43:06 +0100

    Manipulating CSS Styles

--- 03-JavaScript in the Browser DOM and Event Fundamentals/PROJECT 01-Guess My Number!/script.js
+++ 03-JavaScript in the Browser DOM and Event Fundamentals/PROJECT 01-Guess My Number!/script.js
@@ -23,10 +23,18 @@ document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
+  //When there's no input
   if (!guess) {
     document.querySelector(".message").textContent = "No number!🛑";
+
+    //When player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct Number!🎉";
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "It's too high!📈";
@@ -36,13 +44,13 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "You lost the game!😿";
     document.querySelector(".score").textContent = 0;
   } else if (guess < secretNumber) {
-    if (score > 1) {
+    if (score < 1) {
+      //When guess is too low
       document.querySelector(".message").textContent = "It's too low!📉";
       score = score--;
       document.querySelector(".score").textContent = score;
     } else
       document.querySelector(".message").textContent = "You lost the game!😿";
-    document.querySelector(".score").textContent = 0;
     document.querySelector(".score").textContent = 0;
   }
 });
