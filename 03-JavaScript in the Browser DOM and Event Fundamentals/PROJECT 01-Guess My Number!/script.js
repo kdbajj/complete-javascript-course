6515d024f53e6f370c0da2c28bd3381cbec469d3
Karolina Dbaj <k.dbaj@vp.pl>
2022-11-04 21:13:00 +0100

    Implementing Highscores

--- 03-JavaScript in the Browser DOM and Event Fundamentals/PROJECT 01-Guess My Number!/script.js
+++ 03-JavaScript in the Browser DOM and Event Fundamentals/PROJECT 01-Guess My Number!/script.js
@@ -16,6 +16,7 @@ console.log((document.querySelector(".guess").value = 23));
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -33,6 +34,10 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
 
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
     //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
